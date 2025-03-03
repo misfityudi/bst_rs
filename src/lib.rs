655b592698c061acdde8c50b78cd414726f50072
Vikas Chahar <misfityudi@gmail.com>
2025-03-03 20:11:01 +0530

    rename BSTNode struct to BST

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 #[derive(Debug, PartialEq)]
-pub struct BSTNode<T> {
+pub struct BST<T> {
     pub value: T,
-    pub left: Option<Box<BSTNode<T>>>,
-    pub right: Option<Box<BSTNode<T>>>
+    pub left: Option<Box<BST<T>>>,
+    pub right: Option<Box<BST<T>>>
 }
 
-impl<T> BSTNode<T> {
+impl<T> BST<T> {
     pub fn new(value: T) -> Self {
         Self {
             value,
@@ -22,7 +22,7 @@ mod tests {
     #[test]
     fn new() {
         let value = i8::MAX;
-        let node = BSTNode::new(value);
+        let node = BST::new(value);
         assert_eq!(node.value, value);
         assert_eq!(node.left, None);
         assert_eq!(node.right, None);
